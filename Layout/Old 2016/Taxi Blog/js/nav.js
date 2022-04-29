a353455ca858992e7dee79961a6e8f0e2792ab29
igor-mosijchuk <ig.mosijchuk@gmail.com>
2022-04-29 21:42:39 +0300

    clean all

--- Layout/Old 2016/Taxi Blog/js/nav.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-	$('header nav button').click(toggleMenu);
-	function toggleMenu() {
-		$('header nav ul').toggle().addClass('adaptiveMenu');
-		$('header nav ul li').addClass('adaptiveMenuLi');
-	};
-});
