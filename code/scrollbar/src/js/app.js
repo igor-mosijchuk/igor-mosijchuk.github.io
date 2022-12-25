e20b39c6a17727da3b3d34324b84df41f27f7b14
igor-mosijchuk <ig.mosijchuk@gmail.com>
2022-12-25 15:52:40 +0200

    smarthunter

--- code/scrollbar/src/js/app.js
+++ code/scrollbar/src/js/app.js
@@ -1,26 +1,7 @@
 import Scrollbar from 'smooth-scrollbar';
 import { AnchorPlugin } from './modules/anchor-plugin.js';
 import OverscrollPlugin from 'smooth-scrollbar/plugins/overscroll/index.js';
-
-const ScrollbarOptions = {
-	alwaysShowTracks: true,
-	damping: 0.075,
-	plugins: {
-		overscroll: {
-			disableScroll: {
-				direction: 'x',
-			},
-			effect: "bounce",
-			// effect: "glow",
-			// glowColor: "rgba(0,0,0,.2)",
-			enable: true,
-			maxOverscroll: 200,
-			damping: 0.175
-		}
-		// ,overscroll: true
-	}
-};
-
+import { ScrollbarOptions } from './scrollbar-settings/smarthunter.js';
 
 if (window.matchMedia("(min-width: 500px)").matches) {
 	Scrollbar.use(OverscrollPlugin ,AnchorPlugin);
@@ -30,5 +11,3 @@ if (window.matchMedia("(min-width: 500px)").matches) {
 	document.querySelector('.scrollbar-thumb-y').insertAdjacentHTML("beforeend", `<div class="scrollbar-inner"></div>`);
 }
 
-
-projectFunctions.isWebp();
