30f3d03a1ee81028977512e8b982c6f479bc96ac
igor-mosijchuk <ig.mosijchuk@gmail.com>
2023-04-09 23:47:53 +0300

    plus success

--- projects/pages/figma-plugins/js/main.js
+++ projects/pages/figma-plugins/js/main.js
@@ -3,41 +3,48 @@ let SHEET_TITLE = 'Plugins';
 let SHEET_RANGE = 'E1:H150'
 let FULL_URL = ('https://docs.google.com/spreadsheets/d/' + SHEET_ID + '/gviz/tq?sheet=' + SHEET_TITLE + '&range=' + SHEET_RANGE);
 fetch(FULL_URL)
-.then(res => res.text())
-.then(rep => {
-	let data = JSON.parse(rep.substr(47).slice(0,-2));
-	let plugins_list = document.getElementById('plugins-list');
-	let length = data.table.rows.length;
+	.then(res => res.text())
+	.then(rep => {
+		let data = JSON.parse(rep.substr(47).slice(0, -2));
+		let plugins_list = document.getElementById('plugins-list');
+		let length = data.table.rows.length;
 
-	for(let i = 1; i < length; i++){
+		for (let i = 1; i < length; i++) {
 
-		let NewBox = document.createElement('div'),
-			Link = document.createElement('a'),
-			Preview = document.createElement('img'),
-			Info = document.createElement('div'),
-			PluginName = document.createElement('h4'),
-			Description = document.createElement('p');
+			let NewBox = document.createElement('div'),
+				Link = document.createElement('a'),
+				PreviewFrame = document.createElement('div'),
+				Preview = document.createElement('img'),
+				Info = document.createElement('div'),
+				PluginName = document.createElement('h4'),
+				Description = document.createElement('p');
+				StatusField = document.createElement('p');
+			let Status = data.table.rows[i].c[1].v;
+
+			StatusField.innerHTML = Status;
+
+			NewBox.className = `plugins-item ${Status} show`;
+			NewBox.append(Link);
+
+			PluginName.innerHTML = data.table.rows[i].c[0].v;
 
 
-		NewBox.className = "plugins-item";
-		PluginName.innerHTML = data.table.rows[i].c[0].v;
-		Preview.src = "img/preview/" + data.table.rows[i].c[0].v + ".png";
-		Preview.className = "img_style";
-		Link.className = "plugins-item-content";
-		Link.href = data.table.rows[i].c[3].v;
-		Link.target = "_blank"
-		Description.innerHTML = data.table.rows[i].c[2].v;
+			Preview.src = "img/preview/" + data.table.rows[i].c[0].v + ".png";
+			Preview.className = "img_style";
 
-		plugins_list.append(NewBox);
+			Link.className = "plugins-item-content";
+			Link.href = data.table.rows[i].c[3].v;
+			Link.target = "_blank"
+			Link.append(PreviewFrame, Info);
+			Description.innerHTML = data.table.rows[i].c[2].v;
+			Info.className = "plugin-info";
+			Info.append(PluginName, Description);
+			PreviewFrame.append(Preview, StatusField)
+			PreviewFrame.className = "cover"
+			plugins_list.append(NewBox);
+		}
 
-		NewBox.append(Link);
-		Link.append(Preview, Info);
-		Info.className = "plugin-info";
-		Info.append(PluginName, Description)
-	}
-
-})
-
+	});
 
 document.querySelector('#search-input').addEventListener('input', filterList);
 
@@ -54,4 +61,47 @@ function filterList() {
 		}
 	});
 
+}
+
+filterSelection("all")
+function filterSelection(c) {
+	var x, i;
+	x = document.getElementsByClassName("plugins-item");
+	if (c == "all") c = "";
+	for (i = 0; i < x.length; i++) {
+		removeShowClass(x[i], "show");
+		if (x[i].className.indexOf(c) > -1) addShowClass(x[i], "show");
+	}
+}
+
+function addShowClass(element, name) {
+	var i, arr1, arr2;
+	arr1 = element.className.split(" ");
+	arr2 = name.split(" ");
+	for (i = 0; i < arr2.length; i++) {
+		if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+	}
+}
+
+function removeShowClass(element, name) {
+	var i, arr1, arr2;
+	arr1 = element.className.split(" ");
+	arr2 = name.split(" ");
+	for (i = 0; i < arr2.length; i++) {
+		while (arr1.indexOf(arr2[i]) > -1) {
+			arr1.splice(arr1.indexOf(arr2[i]), 1);
+		}
+	}
+	element.className = arr1.join(" ");
+}
+
+// Add active class to the current button (highlight it)
+var btnContainer = document.getElementById("filters");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+	btns[i].addEventListener("click", function(){
+		var current = document.getElementsByClassName("active");
+		current[0].className = current[0].className.replace(" active", "");
+		this.className += " active";
+	});
 }
