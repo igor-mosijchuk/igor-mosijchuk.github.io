4ab3e916ce4d08eb70ecf93f1c00621ec346a4d4
igor-mosijchuk <ig.mosijchuk@gmail.com>
2023-05-08 03:13:40 +0300

    updt

--- projects/boards/macOS/js/keys.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("keydown", e => {
-	if (e.key.toLowerCase() === "u"
-		&& e.ctrlKey
-	) {
-		alert("U key");
-	}
-});
--- projects/boards/macOS/js/scripts.js
+++ projects/boards/macOS/js/scripts.js
@@ -1,28 +1,27 @@
-document.querySelector('.container').onclick = (event) => {
-	const elem = event.target;
-	if (elem.classList.contains('section-symbol')) {
-		console.log(elem);
-		navigator.clipboard.writeText(elem.innerHTML)
-			.then(() => {
-				document.querySelector('.out').innerHTML += 'copy<br>';
-			})
-			.catch(err => {
-				console.log(err);
-			});
+document.getElementById("myElement").style.cssText = `
+	display: block;
+	position: absolute;
+`;
+
+document.addEventListener("keydown", e => {
+	if (e.key.toLowerCase() === "u"
+		&& e.ctrlKey
+	) {
+		alert("U key");
 	}
-}
+});
 
 
-document.querySelector('object#document').onclick = (event) => {
-	const elem = event.target;
-	if (elem.classList.contains('codeNode')) {
-		console.log(elem);
-		navigator.clipboard.writeText(elem.innerHTML)
-			.then(() => {
-				document.querySelector('.out').innerHTML += 'copy<br>';
-			})
-			.catch(err => {
-				console.log(err);
-			});
+
+(function () {
+	var pre = document.querySelectorAll('ul.code pre'),
+		pl = pre.length;
+	for (var i = 0; i < pl; i++) {
+		pre[i].innerHTML = '<div class="line-number"></div>' + pre[i].innerHTML;
+		var num = pre[i].innerHTML.split(/\n/).length;
+		for (var j = 0; j < num - 1; j++) {
+			var line_num = pre[i].getElementsByTagName('div')[0];
+			line_num.innerHTML += '<span>' + (j + 1) + '</span>';
+		}
 	}
-}
+})();
