e500db02ca1a5aba60af995ebc0de61e361c07c1
igor-mosijchuk <ig.mosijchuk@gmail.com>
2023-08-20 11:18:54 +0300

    up2

--- projects/music/playlist-page/src/js/new.js
+++ projects/music/playlist-page/src/js/new.js
@@ -11,5 +11,8 @@ fetch(FULL_URL)
 .then(res => res.text())
 .then(rep => {
 	let data = JSON.parse(rep.substr(47).slice(0, -2));
-	console.log(rep);
+	console.log(FULL_URL);
+	// console.log('https://docs.google.com/spreadsheets/d/1CyHIdo-y5auUGFtUjHnoZwtNjaq0yk1_N4XyV6WCOS4/gviz/tg?sheet=playlist&range=A2:J20');
+	// https://docs.google.com/spreadsheets/d/1CyHIdo-y5auUGFtUjHnoZwtNjaq0yk1_N4XyV6WCOS4/gviz/tg?sheet=playlist&range=A2:J20
+	// console.log(data);
 })
