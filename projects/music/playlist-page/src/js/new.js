cb8ea66a75ec088de7950a57de3865013d153290
igor-mosijchuk <ig.mosijchuk@gmail.com>
2023-08-20 10:52:21 +0300

    up

--- projects/music/playlist-page/src/js/new.js
+++ projects/music/playlist-page/src/js/new.js
@@ -4,7 +4,7 @@
 // console.log('');
 let SHEET_ID = '1CyHIdo-y5auUGFtUjHnoZwtNjaq0yk1_N4XyV6WCOS4';
 let SHEET_TITLE = 'playlist';
-let SHEET_RANGE = 'A1:F100';
+let SHEET_RANGE = 'A2:J20';
 let FULL_URL = ('https://docs.google.com/spreadsheets/d/' + SHEET_ID + '/gviz/tg?sheet=' + SHEET_TITLE + '&range=' + SHEET_RANGE);
 
 fetch(FULL_URL)
