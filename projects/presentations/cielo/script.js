00453d5308db282fdfb616f530e9be3f3d0faf9f
igor-mosijchuk <ig.mosijchuk@gmail.com>
2025-05-05 17:42:49 +0300

    Delete projects/presentations/cielo directory

--- projects/presentations/cielo/script.js
+++
@@ -1,8 +0,0 @@
-const container = document.getElementById("myCarousel");
-const options = { Dots: false };
-
-new Carousel(container, options, { Thumbs });
-
-Fancybox.bind("[data-fancybox]", {
-  // Your custom options
-});
