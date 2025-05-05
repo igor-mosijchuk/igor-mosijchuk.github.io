203d138dc0bf53a7d7ded9e661d3c9343e21cdc8
igor-mosijchuk <ig.mosijchuk@gmail.com>
2025-05-05 22:53:59 +0300

    2025.05.05・22-53

--- presentation/js/script.js
+++ presentation/js/script.js
@@ -1,66 +1,49 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener('DOMContentLoaded', function () {
 
-	const firstHash = "#gallery-1";
-	const filename = window.location.pathname.split("/").pop().slice(0, -5);
+	const carousel = document.getElementById('presentation');
 
-	if (window.location.pathname.endsWith("/" + filename + ".html") && !window.location.hash) {
+	const [h, p] = ['h1', 'p']
+		.map(tag => document.querySelector(`.main-cont .heading ${tag}`)?.textContent || '');
+
+	const firstHash = '#gallery-1';
+	const filename = window.location.pathname.split('/').pop().slice(0, -5);
+
+	if (window.location.pathname.endsWith('/' + filename + '.html') && !window.location.hash) {
 		window.location.hash = firstHash;
 	}
 
-	if (window.location.pathname.endsWith("/presentation/") && !window.location.hash) {
-		window.location.hash = "" + filename + ".html" + firstHash;
+	if (window.location.pathname.endsWith('/presentation/') && !window.location.hash) {
+		window.location.hash = '' + filename + '.html' + firstHash;
 	}
 
 
-	const heading = document.querySelector('.main-cont .heading')
-	const h = heading.querySelector('h1')
-	const p = heading.querySelector('p')
 
-	const container = document.getElementById("myCarousel");
+
 
 	const options = {
 		Dots: false
 	};
 
-	new Carousel(container, options, { Thumbs });
-
-	Fancybox.bind("[data-fancybox]", {
+	new Carousel(carousel, options, { Thumbs });
 
 
+
+	Fancybox.bind('[data-fancybox]', {
+		width: 1280,
+		height: 720,
 		Toolbar: {
-
-			// items: {
-			// 	customContent: {
-			// 		class: "fancybox__custom-content",
-			// 		render: () => {
-			// 			const th = document.querySelector('.fancybox__toolbar__column.is-middle')
-			// 			// console.log(th)
-			// 			th.appendChild(h,p)
-			// 			return `<h3>11212</h3><p>${p}</p>`;
-			// 			// return `<h3>${h}</h3><p>${p}</p>`;
-			// 		}
-			// 	}
-			// },
-
+			items: {
+				heading: {
+					tpl: `<div class="title"><h3>${h}</h3><p>${p}</p></div>`,
+				}
+			},
 
 			display: {
-				// middle: ["zoomIn","customContent"],
-				// middle: ["slideshow"],
-
-				left: ["infobar"],
-				middle: [
-					"zoomIn",
-					"zoomOut",
-					"toggle1to1",
-					"rotateCCW",
-					"rotateCW",
-					"flipX",
-					"flipY",
-				],
-				right: ["fullscreen", "slideshow", "thumbs"]
+				left: ['heading'],
+				middle: ['infobar'],
+				right: ['thumbs', 'slideshow', 'fullscreen']
 			}
 		},
-
 
 		Slideshow: {
 			autoStart: true,
@@ -71,40 +54,18 @@ document.addEventListener("DOMContentLoaded", function () {
 		},
 
 		Carousel: {
-			transition: "classic"
+			transition: 'classic'
 		},
 
 		keyboard: {
 			Escape: null
 		},
+
 		dragToClose: false,
-
-		Toolbar: {
-
-			items: {
-				customContent: {
-					class: "fancybox__custom-content",
-					render: (fancybox, slide) => {
-						const h = "Заголовок";
-						const p = "Опис слайда";
-						return `
-					  <h3>${h}</h3>
-					  <p>${p}</p>
-					`;
-					}
-				}
-			},
-
-			middle: ["customContent"],
-			display: {
-				right: ["fullscreen", "slideshow", "thumbs"],
-			},
-		},
 
 	});
 
 
-	const carousel = document.getElementById('myCarousel');
 
 	const pad = (num, size) => num.toString().padStart(size, '0');
 
